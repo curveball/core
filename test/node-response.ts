d8a3ecc28d6ab2c21d6061340bb7a3ee7a527b37
Evert Pot <me@evertpot.com>
2018-06-25 20:16:52 -0700

    HTTP/2 support for 1xx headers is tested.

--- test/node-response.ts
+++ test/node-response.ts
@@ -1,13 +1,15 @@
 import { expect } from 'chai';
-import { ServerResponse, IncomingMessage } from 'http';
+import http from 'http';
+import http2 from 'http2';
 import { NodeResponse } from '../src/node-response';
 import headersInterfaceTests from './headers-interface-tests';
 import sinon from 'sinon';
+import Application from '../src/application';
 
 function getRes() {
 
-  const request = new IncomingMessage(<any>null);
-  const inner = new ServerResponse(request);
+  const request = new http.IncomingMessage(<any>null);
+  const inner = new http.ServerResponse(request);
 
   inner.setHeader('Content-Type', 'text/html; charset=utf-8');
   inner.statusCode = 200;
@@ -76,15 +78,58 @@ describe('NodeResponse', () => {
       const writeRawMock = mock.expects('_writeRaw');
       writeRawMock.callsArgWith(2, null, true);
 
-      await res.sendInformational(103, { Foo: 'bar'});
+      await res.sendInformational(103, {
+        Foo: 'bar',
+        Many: ['1', '2']
+      });
 
-      const body = `HTTP/1.1 103 Early Hints\r\nFoo: bar\r\n\r\n`;
+      const body = `HTTP/1.1 103 Early Hints\r\nFoo: bar\r\nMany: 1\r\nMany: 2\r\n\r\n`;
 
       expect(writeRawMock.calledOnce).to.equal(true);
       expect(writeRawMock.calledWith(body)).to.equal(true);
 
       mock.restore();
 
+    });
+
+    it('should send a 103 Status when called with a HTTP/2 response', async () => {
+
+
+      const app = new Application();
+      const server = http2.createServer({}, app.callback());
+      let client:any;
+
+      server.listen(8555);
+
+      return new Promise( (resolve, reject) => {
+        app.use(async ctx => {
+
+          await ctx.response.sendInformational(103, {
+            Foo: 'bar',
+            Many: ['1', '2']
+          });
+          ctx.response.body = 'hello';
+
+        });
+
+        client = http2.connect('http://localhost:8555');
+
+        const req = client.request({':path': '/'});
+        req.on('headers', (headers: http2.IncomingHttpHeaders, flags: number) => {
+          resolve(headers);
+        });
+        req.setEncoding('utf-8');
+        req.on('end', () => { client.close(); });
+        req.end();
+      }).then(headers => {
+        client.close();
+        server.close();
+        expect(headers).to.deep.equal({
+          ':status': 103,
+          'foo': 'bar',
+          'many': '1, 2',
+        });
+      });
     });
 
   });
@@ -94,7 +139,7 @@ describe('NodeResponse', () => {
 describe('NodeResponseHeaders', () => {
 
     const res = getRes();
-    headersInterfaceTests(res.headers);    
+    headersInterfaceTests(res.headers);
 
 });
 
