bb217ed8d465e7388b5bdf8c74dbb59fab19be5a
Evert Pot <me@evertpot.com>
2019-12-21 15:16:52 -0500

    Fix H2 push query parameter bug.
    When a resource is pushed with HTTP/2 push, and this resource has a
    query parameter in its path, the parameter will get removed.
    
    The effect is that 'incorrect' resources get pushed, which just get
    ignored by a browser.

--- test/node/push.ts
+++ test/node/push.ts
@@ -105,6 +105,92 @@ describe('NodeResponse http/2 push', () => {
     expect((<any> responseHeaders)[':status']).to.eql(200);
 
   });
+  it('should still work when the pushed resource uses query parameters', async () => {
+
+    const app = new Application();
+    const server = http2.createServer(app.callback());
+    server.listen(32653);
+
+    app.use( ctx => {
+
+      switch (ctx.request.path) {
+        case '/foo' :
+          ctx.response.body = 'Hello world A';
+          ctx.response.push( pushCtx => {
+            pushCtx.request.path = '/bar';
+            return app.handle(pushCtx);
+          });
+          break;
+        case '/bar?sup' :
+          ctx.response.body = 'Hello world B';
+          break;
+      }
+
+    });
+
+    const client = http2.connect('http://localhost:32653', {
+      settings: {
+        enablePush: true,
+      }
+    });
+
+    const req = client.request({':path': '/foo'});
+
+    let data = '';
+    let pushedData = '';
+    let pushRequestHeaders;
+    let pushResponseHeaders;
+    let responseHeaders;
+
+    await new Promise((res, rej) => {
+
+
+      client.on('stream', (pushedStream, requestHeaders) => {
+
+        pushRequestHeaders = requestHeaders;
+
+        pushedStream.setEncoding('utf-8');
+        pushedStream.on('push', (responseHeaders) => {
+
+          pushResponseHeaders = responseHeaders;
+
+        });
+        pushedStream.on('data', (chunk) => {
+
+          pushedData += chunk;
+
+        });
+
+      });
+      req.setEncoding('utf-8');
+      req.on('response', (headers, flags) => {
+        responseHeaders = headers;
+      });
+      req.on('data', (chunk) => {
+        data += chunk;
+      });
+      req.on('end', () => {
+        client.close();
+        res([data, pushedData]);
+      });
+
+    });
+
+    server.close();
+    client.close();
+
+    expect(data).to.equal('Hello world A');
+    expect(pushedData).to.equal('Hello world B');
+    expect(pushRequestHeaders).to.eql({
+      ':authority': 'localhost:32653',
+      ':method': 'GET',
+      ':path': '/bar?sup',
+      ':scheme': 'http',
+    });
+    expect((<any> pushResponseHeaders)[':status']).to.eql(200);
+    expect((<any> responseHeaders)[':status']).to.eql(200);
+
+  });
 
   it('should do nothing when a HTTP/1 server is used', async () => {
 
