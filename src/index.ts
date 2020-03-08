66d6faeaa90b919371d12ca30db6b0389eff2f87
Evert Pot <me@evertpot.com>
2020-03-07 19:13:12 -0500

    Conditional helpers

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,14 @@ import MemoryRequest from './memory-request';
 import MemoryResponse from './memory-response';
 import Request from './request';
 import Response from './response';
+import { conditionalCheck } from './conditional';
 
 export default Application;
 export {
   Application,
   BaseContext,
   Context,
+  conditionalCheck,
   Headers,
   invokeMiddlewares,
   middlewareCall,
