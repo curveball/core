b0415f8421b6bf966984b7c8d4f3a0a073ddfd58
Evert Pot <me@evertpot.com>
2022-04-09 00:35:32 -0400

    Prep 0.18 alpha0 release.

--- src/context.ts
+++ src/context.ts
@@ -194,3 +194,17 @@ export class Context<ReqT = any, ResT = any> {
 
 }
 
+/**
+ * WsContext always has a 'webSocket' property defined.
+ */
+export type WsContext = Context & {
+
+  /**
+   * WebSocket object.
+   *
+   * If the current request is a websocket request, this proprerty will be set
+   *
+   * @see https://github.com/websockets/ws#simple-server
+   */
+  webSocket: WebSocket;
+};
