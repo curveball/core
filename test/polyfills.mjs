ff9a6417b0e1db454aee6eb719670b1513ebad5a
Evert Pot <me@evertpot.com>
2023-02-05 21:15:37 -0500

    Try something else

--- test/polyfills.mjs
+++ test/polyfills.mjs
@@ -1,5 +1,6 @@
+import * as nodeFetch from 'node-fetch';
+
 if (!global.fetch) {
-  const nodeFetch = await import('node-fetch');
   global.fetch = nodeFetch;
   global.Headers = nodeFetch.Headers;
   global.Request = nodeFetch.Request;
